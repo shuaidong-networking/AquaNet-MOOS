1a8864d48072cd999928a483d39f474baf30c8c0
shuaidong-networking <58437879+shuaidong-networking@users.noreply.github.com>
2021-06-20 20:24:34 -0500

    Update pOdometry.h

--- pOdometry/pOdometry.h
+++ pOdometry/pOdometry.h
@@ -1,1 +1,44 @@
+/************************************************************/
+/*    NAME: Shuai Dong                                              */
+/*    ORGN: UA, Tuscaloosa AL                               */
+/*    FILE: Odometry.h                                          */
+/*    DATE: May 3rd, 2021                             */
+/************************************************************/
 
+#ifndef Odometry_HEADER
+#define Odometry_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+
+class Odometry : public AppCastingMOOSApp
+{
+ public:
+   Odometry();
+   ~Odometry();
+
+ protected: // Standard MOOSApp functions to overload  
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+   bool  m_first_reading;
+   double m_current_x;
+   double m_current_y;
+   double m_previous_x;
+   double m_previous_y;
+   double m_total_distance;
+   double m_distanceDiff;
+   double m_requiredDistance;
+ protected: // Standard AppCastingMOOSApp function to overload 
+   bool buildReport();
+
+ protected:
+   void registerVariables();
+
+ private: // Configuration variables
+ void receiveServerMessage();
+ int m_socket = -1;
+ private: // State variables
+};
+
+#endif 
