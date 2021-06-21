21a17e76fcd6d07e74f1d6493a0edc5bc09d8ec2
shuaidong-networking <58437879+shuaidong-networking@users.noreply.github.com>
2021-06-20 20:22:44 -0500

    Update pOdometry.cc

--- pOdometry/pOdometry.cc
+++ pOdometry/pOdometry.cc
@@ -1,1 +1,181 @@
+/************************************************************/
+/*    NAME: Shuai Dong                                              */
+/*    ORGN: UA, Tuscallosa AL                               */
+/*    FILE: OdometrySubcriber.cpp                                        */
+/*    DATE: May 5th, 2021                             */
+/************************************************************/
 
+#include <iterator>
+#include "MBUtils.h"
+#include "ACTable.h"
+#include "OdometrySubscriber.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+//---------------------------------------------------------
+// Constructor
+
+Odometry::Odometry()
+{
+}
+
+//---------------------------------------------------------
+// Destructor
+
+Odometry::~Odometry()
+{
+}
+
+//---------------------------------------------------------
+// Procedure: OnNewMail
+
+bool Odometry::OnNewMail(MOOSMSG_LIST &NewMail)
+{
+  AppCastingMOOSApp::OnNewMail(NewMail);
+
+  MOOSMSG_LIST::iterator p;
+  for(p=NewMail.begin(); p!=NewMail.end(); p++) {
+    CMOOSMsg &msg = *p;
+    string key    = msg.GetKey();
+
+#if 0 // Keep these around just for template
+    string comm  = msg.GetCommunity();
+    double dval  = msg.GetDouble();
+    string sval  = msg.GetString(); 
+    string msrc  = msg.GetSource();
+    double mtime = msg.GetTime();
+    bool   mdbl  = msg.IsDouble();
+    bool   mstr  = msg.IsString();
+#endif
+
+    if(key == "NAV_X")
+    {
+   	 m_previous_x = m_current_x;
+   	 m_current_x = msg.GetDouble();
+    }
+
+    if(key == "NAV_Y")
+	  {
+    	m_previous_y = m_current_y;
+    	m_current_y = msg.GetDouble();
+	  }
+   }
+	
+   return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: OnConnectToServer
+
+bool Odometry::OnConnectToServer()
+{
+   registerVariables();
+  //  Notify( "REQUIRED_ODOMETRY_DIST",total_distance);
+   return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: Iterate()
+//            happens AppTick times per second
+
+bool Odometry::Iterate()
+{
+  AppCastingMOOSApp::Iterate();
+  
+  // Do your thing here!
+  if(m_first_reading == 1)
+  {
+    m_total_distance =sqrt(m_current_x*m_current_x+m_current_y*m_current_y);
+    Notify( "ODOMETRY_DIST1",m_distanceDiff);
+    m_first_reading=0;
+  }
+  else
+  {
+    m_total_distance =sqrt((m_current_x-m_previous_x)*(m_current_x-m_previous_x)+(m_current_y-m_previous_y)*(m_current_y-m_previous_y))+m_total_distance;
+    Notify( "ODOMETRY_DIST1",m_distanceDiff);
+  }
+
+  //AppCastingMOOSApp::PostReport();
+  return(true);
+}
+
+
+
+//---------------------------------------------------------
+// Procedure: OnStartUp()
+//            happens before connection is open
+
+bool Odometry::OnStartUp()
+{
+  AppCastingMOOSApp::OnStartUp();
+  std::cout << "----Shuai OnStarUp()-----" << std::endl;
+  receiveServerMessage();
+  std::cout << "----Shuai  Done()----" << std::endl;
+  
+  
+  STRING_LIST sParams;
+  m_MissionReader.EnableVerbatimQuoting(false);
+  if(!m_MissionReader.GetConfiguration(GetAppName(), sParams))
+    reportConfigWarning("No config block found for " + GetAppName());
+
+  STRING_LIST::iterator p;
+  for(p=sParams.begin(); p!=sParams.end(); p++) {
+    string orig  = *p;
+    string line  = *p;
+    string param = tolower(biteStringX(line, '='));
+    string value = line;
+
+    bool handled = false;
+    if(param == "foo") {
+      handled = true;
+    }
+    else if(param == "bar") {
+      handled = true;
+    }
+
+    if(!handled)
+      reportUnhandledConfigWarning(orig);
+
+  }
+  
+  registerVariables();	
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: registerVariables
+
+void Odometry::registerVariables()
+{
+  AppCastingMOOSApp::RegisterVariables();
+  Register("NAV_X", 0);
+  Register("NAV_Y", 0);
+  // Register("FOOBAR", 0);
+}
+
+
+//------------------------------------------------------------
+// Procedure: buildReport()
+
+bool Odometry::buildReport() 
+{
+  m_msgs << "============================================" << endl;
+  m_msgs << "File:                                       " << endl;
+  m_msgs << "============================================" << endl;
+
+  ACTable actab(4);
+  actab << "Alpha | Bravo | Charlie | Delta";
+  actab.addHeaderLines();
+  actab << "one" << "two" << "three" << "four";
+  m_msgs << actab.getFormattedString();
+
+  return(true);
+}
